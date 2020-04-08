0c85b4d3aad5b8e649e42b65e0275c9f61898d83
ObliqueOne <60855535+obliqueone-git@users.noreply.github.com>
2020-04-08 17:46:21 -0400

    initial commit

---
+++ devopsdemo/src/test/java/com/devops/phoenix/devopsdemo/DevopsdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.phoenix.devopsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
